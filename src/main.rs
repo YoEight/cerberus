33164b216b0eec4744982584c4dd95306bb70767
YoEight <yo.eight@gmail.com>
2019-07-31 23:36:48 +0200

    Implement check command.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use futures::future::Future;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -6,18 +7,57 @@ struct Args {
     #[structopt(long = "host", short = "h", default_value = "127.0.0.1", help = "Database host.")]
     host: String,
 
-    #[structopt(long = "port", short = "p", default_value = "1113", help = "Database TCP port.")]
+    #[structopt(long = "tcp-port", short = "p", default_value = "1113", help = "Database TCP port.")]
     port: u16,
+
+    #[structopt(long = "http-port", default_value = "2113", help = "Database public HTTP port.")]
+    http_port: u16,
 
     #[structopt(long = "login", help = "User login.")]
     login: Option<String>,
 
     #[structopt(long = "password", help = "User password.")]
     password: Option<String>,
+
+    #[structopt(subcommand)]
+    cmd: Cmd,
+}
+
+#[derive(Debug, StructOpt)]
+enum Cmd {
+    #[structopt(name = "check", help = "Checks if a database node is reachable.", about = "Checks if a database node is reachable.")]
+    Check,
 }
 
 fn main() {
     let args = Args::from_args();
 
-    println!("{:?}", args);
+    match args.cmd {
+        Cmd::Check => {
+            let addr = format!("{}:{}", args.host, args.port).parse().unwrap();
+            let connection = eventstore::Connection::builder()
+                .connection_retry(eventstore::Retry::Only(0))
+                .single_node_connection(addr);
+
+            let result = connection.read_all()
+                .start_from_beginning()
+                .max_count(1)
+                .execute()
+                .wait();
+
+            if let Err(e) = result {
+                if let eventstore::OperationError::Aborted = e {
+                    eprintln!(
+                        "Failed to connect to node {}:{} through its public TCP port.",
+                        args.host, args.port);
+
+                    return;
+                }
+            }
+
+            println!(
+                "Successfully connected to node {}:{} through its public TCP port.",
+                args.host, args.port);
+        }
+    }
 }
